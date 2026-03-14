3365b799820602d72ce6790769c76c6762d61d8a
just-tenacious <tshraddha1505@gmail.com>
2026-03-14 14:12:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Online cloud deployment on netlify</h1>
+      <p>
+        Thias is a sample website
+      </p>
+    </>
+  )
+}
+
+export default App
