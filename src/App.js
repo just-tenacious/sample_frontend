8e30313b7021a94730ee867fcab8fc25db9ebd6b
just-tenacious <tshraddha1505@gmail.com>
2026-04-17 11:38:51 +0530

    Push code

--- src/App.js
+++ src/App.js
@@ -4,9 +4,41 @@ const App = () => {
   return (
     <>
       <h1>Online cloud deployment on netlify of Library Subscription System</h1>
-      <p>
-        Thias is a sample website
-      </p>
+      <form>
+        <div>
+          <label>
+            Enter Id
+          </label>
+          <input type='number' />
+        </div>
+        <div>
+          <label>
+            Enter Full name
+          </label>
+          <input type='text' />
+        </div>
+        <div>
+          <label>
+            Enter Email
+          </label>
+          <input type='email' />
+        </div>
+        <div>
+          <label>
+            Enter Password
+          </label>
+          <input type='password' />
+        </div>
+        <div>
+          <label>
+            Enter Months For
+          </label>
+          <input type='number' />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
     </>
   )
 }
